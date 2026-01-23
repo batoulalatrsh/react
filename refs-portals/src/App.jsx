3bb3035ce328a1e787d954ce12e2d3c8460bc11e
batoulalatrsh <batoulalatrsh76@gmail.com>
2026-01-23 05:36:29 +0200

    edits

--- refs-portals/src/App.jsx
+++ refs-portals/src/App.jsx
@@ -1,14 +1,16 @@
-import Player from "./components/Player.jsx";
-import TimerChallenge from "./components/TimerChallenge.jsx";
+import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 function App() {
   return (
     <>
       <Player />
       <div id="challenges">
-        <TimerChallenge title="Easy" targetTime={1} />
-        <TimerChallenge title="Not easy" targetTime={5} />
-        <TimerChallenge title="Getting tough" targetTime={10} />
-        <TimerChallenge title="pros only" targetTime={15} />
+        <TimerChallenge title='Easy' targetTime={1}/>
+        <TimerChallenge title='Not easy' targetTime={5}/>
+        <TimerChallenge title='Getting tough' targetTime={10}/>
+        <TimerChallenge title='pores good' targetTime={15}/>
+
+        
       </div>
     </>
   );
--- refs-portals/src/components/TimerChallenge.jsx
+++ refs-portals/src/components/TimerChallenge.jsx
@@ -1,17 +1,38 @@
+import { useState, useRef } from "react";
+
+// let timer; => we cant use it because when we click two instance i n one time first onre will be not executed
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();//when we rerender the component value dont lost
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStart, setTimerStart] = useState(false);
+
+  //let timer; =>we cant use it because when we click stop component will be rerendered and timer be undefined
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStart(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>
-            Start Challenge
+        <button onClick={timerStart ? handleStop : handleStart}>
+          {timerStart ? "stop" : "Start"} Challenge
         </button>
       </p>
-      <p className="">
-        Time is runing... /
+      <p className={timerStart ? "active" : undefined}>
+        {timerStart ? "Time is running..." : "Timer inactive"}
       </p>
     </section>
   );
