3a9d15226a71b4df44fd9ae4ec361953d9af6649
batoulalatrsh <batoulalatrsh76@gmail.com>
2026-01-24 04:03:43 +0200

    edits

--- refs-portals/src/components/ResultModal.jsx
+++ refs-portals/src/components/ResultModal.jsx
@@ -1,16 +1,35 @@
-export default function ResultModal({ ref, result, targetTime }) {
+import { forwardRef, useImperativeHandle, useRef } from "react"; //use to support ref prop in old browsers
+const ResultModal = forwardRef(function ResultModal(
+  { result, targetTime, onReset },
+  ref,
+) {
+  const dialog = useRef();
+
+  const userLost = result <= 0;
+  const formattedRemainingTime = (result / 1000).toFixed(2);
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
   return (
-    <dialog ref={ref} className="result-modal" >
-      <h2>Your {result}</h2>
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>You lost!</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
-      <p> 
-        You stopped the timer with <strong>X seconds left.</strong>
+      <p>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form action="dialog">
+      <form action="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
   );
-}
+});
+export default ResultModal;
--- refs-portals/src/components/TimerChallenge.jsx
+++ refs-portals/src/components/TimerChallenge.jsx
@@ -5,40 +5,54 @@ import ResultModal from "./ResultModal";
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef(); //when we rerender the component value dont lost
   const dialog = useRef();
-  const [timerExpired, setTimerExpired] = useState(false);
-  const [timerStart, setTimerStart] = useState(false);
 
+  // const [timerExpired, setTimerExpired] = useState(false);
+  // const [timerStart, setTimerStart] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
   //let timer; =>we cant use it because when we click stop component will be rerendered and timer be undefined
-  function handleStart() {
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-      dialog.current.showModal();
-    }, targetTime * 1000);
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
 
-    setTimerStart(true);
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.open();
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
+
+  function handleStart() {
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
+    }, 10);
   }
 
   function handleStop() {
-    clearTimeout(timer.current);
+    dialog.current.open();
+    clearInterval(timer.current);
   }
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        result={timeRemaining}
+        onReset={handleReset}
+      />
 
       <section className="challenge">
         <h2>{title}</h2>
-        {timerExpired && <p>You lost!</p>}
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStart ? handleStop : handleStart}>
-            {timerStart ? "stop" : "Start"} Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={timerStart ? "active" : undefined}>
-          {timerStart ? "Time is running..." : "Timer inactive"}
+        <p className={timerIsActive ? "active" : undefined}>
+          {timerIsActive ? "Time is running..." : "Timer inactive"}
         </p>
       </section>
     </>
