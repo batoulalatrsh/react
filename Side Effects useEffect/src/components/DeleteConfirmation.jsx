39383f6dd95dddd2956bd8daeeb19e80db04e0fe
batoulalatrsh <batoulalatrsh76@gmail.com>
2026-01-29 02:05:03 +0200

    edits

--- Side Effects useEffect/src/components/DeleteConfirmation.jsx
+++ Side Effects useEffect/src/components/DeleteConfirmation.jsx
@@ -2,10 +2,15 @@ import { useEffect } from "react";
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
   useEffect(() => {
     console.log("TIMER SET");
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       onConfirm();
     }, 3000);
-  }, []);
+
+    return () => {
+      console.log("clean");
+      clearTimeout(timer);
+    };
+  }, [onConfirm]);
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
