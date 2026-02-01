5ac9fc2701d908192a0c58c43b4b4055e0554d36
batoulalatrsh <batoulalatrsh76@gmail.com>
2026-02-01 22:36:26 +0200

    edits

---
+++ React behind the scene/src/components/explanations.js
@@ -0,0 +1,7 @@
+/*
+-jsx code convert to js and then to dom element
+-memo function compare cur props with last probs and if they change reexecute the function
+-memeo reExecute the the 'component' if 1) robs change 2) internal state change
+-useMemo prevent re execute of 'function' if the parameter NOT change
+
+*/
