d84f5b37268c4e9ceb88f80982cc85bf94cca31e
batoulalatrsh <batoulalatrsh76@gmail.com>
2026-02-16 23:55:45 +0200

    updates

--- Redux/src/store/index.js
+++ Redux/src/store/index.js
@@ -1,33 +1,57 @@
 // const redux = require("redux");
 import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
 const initialState = { counter: 0, showCounter: true };
-function counterReducer(state = initialState, action) {
-  if (action.type === "increment") {
-    return { counter: state.counter + 1, showCounter: state.showCounter };
-  }
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
+// function counterReducer(state = initialState, action) {
+//   if (action.type === "increment") {
+//     return { counter: state.counter + 1, showCounter: state.showCounter };
+//   }
 
-  if (action.type === "decrement") {
-    return { counter: state.counter - 1, showCounter: state.showCounter };
-  }
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
 
-  if (action.type === "toggle") {
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter,
-    };
-  }
+//   if (action.type === "decrement") {
+//     return { counter: state.counter - 1, showCounter: state.showCounter };
+//   }
 
-  return state;
-}
+//   if (action.type === "toggle") {
+//     return {
+//       counter: state.counter,
+//       showCounter: !state.showCounter,
+//     };
+//   }
 
-const store = createStore(counterReducer);
+//   return state;
+// }
+
+// const store = createStore(counterSlice.reducer);
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
 // //Subscriber with data store
 // function counterSubscriber() {
